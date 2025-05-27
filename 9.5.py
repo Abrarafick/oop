2a71ace7c3f3f92a58e658580f75b7ed8acae895
Abrarafick <mdsaadabrar2024@gmail.com>
2025-05-27 22:21:11 +0600

    hu

--- 9.5.py
+++ 9.5.py
@@ -1,14 +1,13 @@
 from noy_point_three import user 
 
-class user1(user):
-    def increment_login_attemp(self):
-        self.login_attemps+=1
+def increment_login_attemp(self):
+    self.login_attemps+=1
         
-    def reset_log_in_method(self):
-        self.login_attemps=0
+def reset_log_in_method(self):
+    self.login_attemps=0
     
-    def login_attemps_method(self):
-        printf(f"The number of log in attemps are {self.login_attemps}")
+def login_attemps_method(self):
+       printf(f"The number of log in attemps are {self.login_attemps}")
         
 ami.increment_login_attemp()
 ami.increment_login_attemp()
